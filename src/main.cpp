55297fa548530188c77479e6e3c7d3af42aaf955
gbooth <g.booth@uleth.ca>
2020-04-06 22:56:41 -0600

    mistakes suck

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@ int main () {
     Board_Tile test1("023145786", "UU");
     Board_Tile test2("123405786", "UR");
 
+    test1.Manhattan_Distance();
+    test2.Manhattan_Distance();
+
     if(test1 < test2)
         std::cout << "test1 is smaller than test2 and I dun fked up\n\n";
     else
